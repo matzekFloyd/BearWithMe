27779e219fbff94fd1dbce70a3817f93d96bf991
Mathias Mayrhofer <mathiaspmayrhofer@gmail.com>
2021-09-26 12:00:22 +0200

    adds header, main, footer base html structure

--- src/js/App.js
+++ src/js/App.js
@@ -1,24 +1,12 @@
-import logo from '../logo.svg';
-import '../sass/main.scss';
+import "../sass/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header id={"app-header"}>Header</header>
+      <main id={"app-main"}>Content</main>
+      <footer id={"app-footer"}>Footer</footer>
+    </>
   );
 }
 
--- src/js/App.test.js
+++ src/js/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let header = document.getElementById('app-header');
+  expect(header).toBeInTheDocument();
+});
+
+test("renders main", () => {
+  render(<App />);
+  let main = document.getElementById('app-main');
+  expect(main).toBeInTheDocument();
+});
+
+test("renders header", () => {
+  render(<App />);
+  let footer = document.getElementById('app-footer');
+  expect(footer).toBeInTheDocument();
 });
