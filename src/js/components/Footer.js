56bca66c3233fd962d4e201467046a3f386a59be
Mathias Mayrhofer <mathiaspmayrhofer@gmail.com>
2021-10-16 23:25:52 +0200

    some styling (wip)

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -2,5 +2,8 @@
  * @this Footer
  */
 export function Footer() {
-  return <footer id={"app-footer"}>Built with my own bear hands © 2021 Mathias Mayrhofer </footer>;
+  return <footer id={"app-footer"}>
+    <div/>
+    <span className={"app-footer__copy-right"}>Built with my own bear hands © 2021 Mathias Mayrhofer</span>
+  </footer>;
 }
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,5 +2,8 @@
  * @this Header
  */
 export function Header() {
-  return <header id={"app-header"}/>;
+  return <header id={"app-header"}>
+    <span className={"app-header__disclaimer"}>A bear a day, to keep your sorrows away... ☺</span>
+    <div/>
+  </header>;
 }
