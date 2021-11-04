e9582852e6f159a1ff5fa34fa06456254d7139d8
Mathias Mayrhofer <mathiaspmayrhofer@gmail.com>
2021-11-04 20:22:51 +0100

    minor text change

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -7,7 +7,7 @@ export function Header() {
   return (
     <header id={"app-header"}>
       <p className={"app-header__disclaimer"}>
-        For my lovely Babsi bear: A bear a day, to keep your sorrows away...{" "}
+        For my lovely Babsi bear. A bear a day, to keep your sorrows away...{" "}
         <img src={Heart} alt={"A heart"} />
       </p>
     </header>
