ee90876c7e584efff7d60de76d860b78c25f3bd7
Mathias Mayrhofer <mathiaspmayrhofer@gmail.com>
2021-11-04 19:51:18 +0100

    dedication styling

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,9 +1,15 @@
+import Heart from "../../images/black_heart_24.png";
+
 /**
  * @this Header
  */
 export function Header() {
-  return <header id={"app-header"}>
-    <span className={"app-header__disclaimer"}>A bear a day, to keep your sorrows away... ☺</span>
-    <div/>
-  </header>;
+  return (
+    <header id={"app-header"}>
+      <p className={"app-header__disclaimer"}>
+        For my lovely Babsi bear: A bear a day, to keep your sorrows away...{" "}
+        <img src={Heart} alt={"A heart"} />
+      </p>
+    </header>
+  );
 }
